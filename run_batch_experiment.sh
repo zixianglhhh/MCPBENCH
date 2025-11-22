c14e815b3312cbb1f8f6b6cb06a8b3b8db7a01b0
carenceTencentLightTokyo1 <liuwenrui@pku.edu.cn>
2025-11-22 20:51:14 +0800

    rewrite deepseek

--- run_batch_experiment.sh
+++ run_batch_experiment.sh
@@ -6,6 +6,10 @@
 # bash ./run_one_model_experiment.sh qwen3-235b-a22b-instruct-2507
 # bash ./run_one_model_experiment.sh openai/gpt-5
 # bash ./run_one_model_experiment.sh x-ai/grok-4
-bash ./run_one_model_experiment.sh openai/o4-mini
-bash ./run_one_model_experiment.sh openai/o3
+# bash ./run_one_model_experiment.sh openai/o4-mini
+# bash ./run_one_model_experiment.sh openai/o3
 # bash ./run_one_model_experiment.sh gemini-2.5-pro
+# bash ./run_one_model_experiment.sh qwen/qwen3-1.7b
+bash ./run_one_model_experiment.sh qwen/qwen3-8b
+bash ./run_one_model_experiment.sh qwen/qwen3-14b
+bash ./run_one_model_experiment.sh qwen/qwen3-32b
